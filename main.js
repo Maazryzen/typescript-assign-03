36fc7c9e6643d5a8fba38b43f25bb8e00c9762f5
maaz khan <maazwarid289@gmail.com>
2024-04-19 19:53:34 -0700

    first assign 01

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("hello world! project 007");
+console.log("hello world! project");
+console.log("hello world! project Maaz 007");
---
+++ main.ts
@@ -0,0 +1,5 @@
+console.log("hello world! project 007");
+
+console.log("hello world! project");
+
+console.log("hello world! project Maaz 007");
